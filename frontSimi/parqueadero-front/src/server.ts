d89429e7ef3b453391ab09e919a99b56db90114f
Vicente Silva <tuemail@example.com>
2026-05-28 03:50:56 -0500

    Remove exposed firebase credentials

--- frontSimi/parqueadero-front/src/server.ts
+++ frontSimi/parqueadero-front/src/server.ts
@@ -63,6 +63,6 @@ if (isMainModule(import.meta.url) || process.env['pm_id']) {
 }
 
 /**
- * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
+ * Request handler used by the Angular CLI (for dev-server and during build) or rm -rf frontSimi/parqueadero-front/.gitFirebase Cloud Functions.
  */
 export const reqHandler = createNodeRequestHandler(app);
